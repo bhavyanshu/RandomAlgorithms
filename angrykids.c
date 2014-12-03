a1f939091b3e07d4c7740c4de7f988f64d12cc9e
bhavyanshu <bhavyanshu.spl@gmail.com>
2014-12-03 23:13:44 +0530

    Reduced time complexity

--- angrykids.c
+++ angrykids.c
@@ -10,19 +10,6 @@ int compare(const void *a,const void *b){
 	return (*da>*db) - (*da<*db);
 }
 
-long long int find_max(int *ar,long long int init_elem,long long int size){
-	long long int l,m;
-	m=ar[init_elem];
-	for(l=init_elem;l<size;l++)
-	{
-		if(ar[l]>m)
-		{
-		m = ar[l];
-		}
-	}
-	return m;
-}
-
 int main(){
 	long long int i,j,k,n,z,c;
 	long long int unfairness=999999999999,new_unfairness;
@@ -37,19 +24,17 @@ int main(){
 	//for(i=0;i<n;i++)
 		//scanf("%lld",&arr[i]);
 	int q=0;
-	static const char filename[] = "angrykidstestcase.txt";
+	static const char filename[] = "angrykidstestcase2.txt";
 	FILE *file = fopen(filename, "r");
 	if ( file != NULL )
 	{
 	int buffer;
-		//while (fgets(line, sizeof (line), file)!=NULL) /* read a line */
-		//{
 		while(fscanf(file,"%d",&buffer)!=EOF) {
 		arr[q]=buffer;
 		q++;
 		}
 	}
-	else 
+	else
 		printf("Error opening file!\n");
 
 	for(c=0;c<n;c++) {
@@ -58,14 +43,11 @@ int main(){
 
 	qsort(arr,n, sizeof(int),compare); //Sort in ascending order
 
-	for(i=0;i<n-k;i++){
-		z=k+i;
+	for(i=0;i<=n-k;i++){
+		z=k+i-1;
 		if(z>(n-1)){ break;}
-		//printf("\ni=%d",i);
 		min=arr[i];
-		//printf("Min:%d",min);
-		max=find_max(arr,i,z);
-		//printf("Max:%d\n",max);
+		max=arr[z];
 		new_unfairness = max-min;
 		if(new_unfairness<unfairness){
 			//Update unfairness
